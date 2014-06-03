7709d57a575fe4f94af964b9d34ec160a2e0c188
Void Main <voidmain1313113@gmail.com>
2014-06-04 07:49:22 +0800

    Initial Commit

---
+++ SwiftWithCocoa/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftWithCocoa
+//
+//  Created by Sun Peng on 6/4/14.
+//  Copyright (c) 2014 Void Main. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftWithCocoa/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SwiftWithCocoa
+//
+//  Created by Sun Peng on 6/4/14.
+//  Copyright (c) 2014 Void Main. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
